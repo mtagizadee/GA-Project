6eaa6f7e6b674204d27a61060b363e2c38767ba9
mtagizadee <magatagi0@gmail.com>
2023-04-16 17:27:09 +0400

    destinations class was added

---
+++ src/Destination.java
@@ -0,0 +1,21 @@
+public class Destination {
+    private String name;
+    private int id;
+
+    public Destination(String name, int id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
